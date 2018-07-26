0ca270fa60a6a8051a8c5ef74203bff942ee8b42
TheFSilver <dasylva.f@gmail.com>
2018-07-26 18:30:14 +0200

    First commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+3.times do
+  city = City.create(city_name: Faker::GameOfThrones.city)
+end
+
+15.times do
+  dogsitter = Dogsitter.create(name: Faker::GameOfThrones.character, city_id: City.all.sample.id)
+
+end
+
+45.times do
+  dog = Dog.create(name: Faker::Dog.name, city_id: City.all.sample.id)
+
+end
+
+City.all.each do |cityname|
+  4.times do
+  randomdog = Dog.all.sample
+  randomsitter = Dogsitter.all.sample
+  
+  while randomdog.city_id != randomsitter.city_id
+    randomdog = Dog.all.sample
+    randomsitter = Dogsitter.all.sample
+  end
+  stroll = Stroll.create(location: , city_id: cityname.id, dogsitter_id: randomdog.id, dog_id: randomsitter.id)
+  end
+end
